16eff5e3d780f4f934a4bec4e2dc8190ccc47b23
Tony Eneh <aeneh90@gmail.com>
2020-06-19 17:14:50 +0100

    feat: write channels' list component

--- src/Components/ChannelsList/ChannelsList.js
+++ src/Components/ChannelsList/ChannelsList.js
@@ -1,10 +1,71 @@
-import React from 'react';
+import React from "react";
+import Header from "../Header/Header";
+import { Link } from "react-router-dom";
+import favouriteIcon from "../../images/love-icon.png";
 
-export default class ChannelsList extends React.Component{
+export default class ChannelsList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      error: null,
+      channels: [],
+    };
+  }
 
-    render(){
-        return (
-            <div>oti work ChannelsList</div>
-        )
-    }
+  componentDidMount() {
+    fetch("http://localhost:4200/api/channels")
+      .then((result) => result.json())
+      .then((data) => {
+        this.setState({
+          channels: data,
+          isLoading: false,
+        });
+      })
+      .catch((err) =>
+        this.setState({
+          error: err,
+          isLoading: false,
+        })
+      );
+  }
+
+  render() {
+    return (
+      <>
+        <Header></Header>
+
+        <section className="main">
+          {/* <!-- TODO put placeholder image in video element below --> */}
+          <div className="channels-title">
+            <span>CHANNELS</span>
+            <span className="channels-list-style-toggle"></span>
+          </div>
+          <ul className="channels-list">
+            {this.state.isLoading && <li>Loading...</li>}
+            {this.state.error && (
+              <li>
+                An Error occured while Loading your channels. Try refreshing the
+                browser
+              </li>
+            )}
+            {!this.state.isLoading &&
+              !!this.state.channels.length &&
+              this.state.channels.map((channel) => {
+                return (
+                  <li key={channel.id}>
+                    <Link to={{ pathname: `/channels/${channel.id}` }}>
+                      {channel.name}
+                    </Link>
+                    <Link to="" onclick="" className="toggle-favourites">
+                      <img src={favouriteIcon} alt="" />
+                    </Link>
+                  </li>
+                );
+              })}
+          </ul>
+        </section>
+      </>
+    );
+  }
 }
